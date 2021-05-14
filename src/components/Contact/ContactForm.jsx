51fe283e7a02034bf2d1b3a22f129b458a584675
Ruth Anne <anne.carvalho@yahoo.com.br>
2021-05-14 20:22:11 -0300

    Disabled submit

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -24,20 +24,30 @@ export default class ContactForm extends React.Component {
     this.setState({ [name]: value });
   }
 
+  disableFields(){
+    document.getElementById('submit').disabled = true;
+  }
+
   sendMessage(event) {
     event.preventDefault();
 
     if (this.state.name === (null || '') || this.state.email === (null || '') || 
         this.state.content === (null || '')) {
-      toast.warning('Preencha todos os campos', {autoClose:4000})
+      toast.warning('Preencha todos os campos', {
+        autoClose:4000, 
+        position: toast.POSITION.TOP_CENTER})
       return false;
     }
 
     var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
     if (!pattern.test(this.state.email)) {
-      toast.warning('E-mail inválido', {autoClose:4000})
+      toast.warning('E-mail inválido', {
+        autoClose:4000, 
+        position: toast.POSITION.TOP_CENTER})
       return false;
     }
+
+    this.disableFields();
 
     // const templateParams = {
     //   from_name: this.state.name + " (" + this.state.email + ")",
@@ -48,13 +58,18 @@ export default class ContactForm extends React.Component {
     // const env = process.env || {}
     // emailjs.send(env.REACT_APP_EMAIL_SERVICE_ID, env.REACT_APP_EMAIL_TEMPLATE_ID, templateParams, env.REACT_APP_EMAIL_USER_ID)
     //   .then(function (response) {
-    //     toast.success("Sua mensagem foi enviada!", {
+    //     toast.success("Mensagem enviada com sucesso!", {
     //       position: toast.POSITION.TOP_CENTER
     //     });
     //     console.log("SUCESSO!", response.status, response.text);
+    //       return true;
     //   },
     //     function (err) {
-    //       toast.error("Não foi possível enviar a sua mensagem.");
+    //       toast.error('Não foi possível enviar a sua mensagem.', {
+    //         autoClose:4000,  
+    //         position: toast.POSITION.TOP_CENTER
+    //     });
+    //       return false;
     //     }
     //   );
 
@@ -63,7 +78,9 @@ export default class ContactForm extends React.Component {
       email: "",
       content: ""
     });
-    console.log("Email enviado com sucesso!")
+        toast.success("Mensagem enviada com sucesso!", {
+          position: toast.POSITION.TOP_CENTER
+        });
   }
 
   render() {
@@ -84,12 +101,12 @@ export default class ContactForm extends React.Component {
                 <input
                   id="name"
                   name="name"
+                  className="input"
                   type="text"
                   onChange={this.handleInputChange.bind(this)}
                   required
                   value={this.state.name}
                   rows={1}
-                  className="input"
                 />
             </div>
             <div className="field">
@@ -99,13 +116,13 @@ export default class ContactForm extends React.Component {
                 <input
                   id="email"
                   name="email"
+                  className="input"
                   type="text"
                   onChange={this.handleInputChange.bind(this)}
                   required
                   value={this.state.email}
                   //error={this.state.errors.email}
                   rows={1}
-                  className="input"
                 />
               </div>
           </div>
@@ -133,9 +150,11 @@ export default class ContactForm extends React.Component {
             <div className="field">
               <ToastContainer />
               <input
-                type="button"
-                value="Enviar"
-                className="submit"
+                id='submit'
+                name='submit'
+                type='button'
+                value='Enviar'
+                className='submit'
                 onClick={this.sendMessage.bind(this)}
               />
             </div>
