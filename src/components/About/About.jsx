929dedca066b72dcc30d4e98297d8b4760801e9f
Ruth Anne <anne.carvalho@yahoo.com.br>
2021-05-20 15:40:07 -0300

    About page [1/2]

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,33 @@
 import React from "react";
+import './About.scss';
 
 const About = () => (
-  <div>
-    <h1>This is the About Page</h1>
-    <p>
-      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-      inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus
-      et ultrices posuere cubilia curae; Duis consequat nulla ac ex consequat,
-      in efficitur arcu congue. Nam fermentum commodo egestas.
-    </p>
-  </div>
+
+    <div className="div-all">
+        <h1 id="h1-title">Sobre</h1>
+        <div className='div-left'>
+        <p id="content">
+            Meu nome é Ruth Anne, resido em Salvador-BA, tenho 26 anos e atualmente trabalho como desenvolvedora 
+            front-end na <a id="ref" href="https://solutis.com.br/" rel="noopener noreferrer" target="_blank">Solutis Tecnologias</a>. 
+            Sou tecnóloga em Análise e Desenvolvimento de Sistemas (2017) e bacharela em Engenharia de Software (2020), pela <a id="ref"
+            href="https://www.ucsal.br/" rel="noopener noreferrer" target="_blank"> Universidade Católica do Salvador - UCSAL</a>.
+        </p>
+        <br/>
+        <p id="content">
+            Ainda na universidade, desenvolvi uma paixão pela área de Ciências de Dados, na qual tenho projetos e dedicação 
+            no tempo livre, focando também em tecnologias que envolvam Machine Learning.
+        </p>
+        <br/>
+        <p id="content">
+            Sou uma pessoa bem organizada, proativa, detalhista, naturalmente curiosa e me empenho perpetuamente para melhorar 
+            as minhas habilidades. Tenho dois gatos (Lunna e Kowalski), amo viajar e sou apaixonada por quadrinhos.
+        </p>
+        <br/>
+        <p id="content">
+            Caso eu possa te ajudar em algo, <a id="ref" rel="noopener noreferrer" href='/contact'>pode me chamar</a>!
+        </p>
+        </div>
+    </div>
 );
 
 export default About;
