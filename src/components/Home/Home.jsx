d3a534095aae586f44847eb2c3ccab9929025521
Ruth Anne <anne.carvalho@yahoo.com.br>
2021-05-24 12:30:01 -0300

    Datacamp and HackerRank icons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import MeImg from "../../assets/me.png"
 import './Home.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpotify, faGithubAlt, faLinkedin, faTelegramPlane } from "@fortawesome/free-brands-svg-icons";
+import { faSpotify, faGithubAlt, faTelegramPlane, faHackerrank } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { LinkedinBrandIcon, DatacampBrandIcon } from '@datacamp/waffles-icons';
 import CustomScrollbar from '../CustomScrollbar/CustomScrollbar';
 
 const Home = () => (
@@ -23,7 +24,13 @@ const Home = () => (
                     <FontAwesomeIcon icon={faGithubAlt} />
                 </a>
                 <a rel="noopener noreferrer" href='https://www.linkedin.com/in/anne-carvalho/' target="_blank" title="LinkedIn">
-                    <FontAwesomeIcon icon={faLinkedin} />
+                    <LinkedinBrandIcon size={30} />
+                </a>
+                <a rel="noopener noreferrer" href='https://www.datacamp.com/profile/annecarvalho' target="_blank" title="Datacamp">
+                    <DatacampBrandIcon size={30}/>
+                </a>
+                <a rel="noopener noreferrer" href='https://www.hackerrank.com/anne_carvalho' target="_blank" title="HackerRank">
+                    <FontAwesomeIcon icon={faHackerrank} />
                 </a>
                 <a rel="noopener noreferrer" href='https://open.spotify.com/user/anne.carvalho/' target="_blank" title="Spotify">
                     <FontAwesomeIcon icon={faSpotify} />
