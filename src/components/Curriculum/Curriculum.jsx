db6734e0d0b130e92239dbcb99dc294736d6a1c9
Ruth Anne <anne.carvalho@yahoo.com.br>
2021-05-25 15:55:15 -0300

    Curriculum content

--- src/components/Curriculum/Curriculum.jsx
+++ src/components/Curriculum/Curriculum.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import CustomScrollbar from '../CustomScrollbar/CustomScrollbar';
+import { Button } from '@material-ui/core';
+import cvpt from "../../documents/CV-PT_Ruth-Anne.pdf";
+import cven from "../../documents/CV-EN_Ruth-Anne.pdf";
 
 const Curriculum = () => (
   <CustomScrollbar>
     <div className="div-all">
-      <h1 id="h1-title">Curriculum Page</h1>
-      <p>
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-        inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus
-        et ultrices posuere cubilia curae; Duis consequat nulla ac ex consequat,
-        in efficitur arcu congue. Nam fermentum commodo egestas.
-      </p>
+      <div className="div-left">
+        <h1 id="h1-title">Currículo</h1>
+      </div>
+      <div className="div-right">
+        <p id="content">
+          <Button variant="outlined" href={cven} target="_blank">Download EN-EN</Button>
+        </p>
+      </div>
+      <br />
+      <iframe
+        className="responsive-iframe"
+        src={`${cvpt}#zoom=110`}
+        type="application/pdf"
+        title="CV"
+        style={{
+          borderRadius: 5,
+          border: 0,
+          width: '100%',
+          height: 530
+        }} />
     </div>
   </CustomScrollbar>
 );
